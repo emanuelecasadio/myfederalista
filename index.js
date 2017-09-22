8db18e2298d9bb0b11f533ecac9eb66f3eea912a
Emanuele Casadio <emanuele.casadio@kopjra.com>
2017-09-22 12:32:35 +0200

    Final fixes

--- index.js
+++ index.js
@@ -11,32 +11,33 @@ mongoose.connect(process.env.MONGODB_URI);
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', {});
-});
+app.get(/^\/[^.]*$/, (req, res) => {
+  let name = req.query.name || req.path.substring(1);
+  name = name.trim();
 
-app.get(/^\/cm/, (req, res) => {
-  let name = req.query.name || req.path.substring(4);
-
-  Person.findOne({name}).exec()
-  .then(person => {
-    if(!person){
-      person = new Person({
-        name,
-        cm: getRandomArbitrary(10, 30)
-      });
-      person.save()
-      .then(()=>{;})
-      .catch(err=>{console.error(err)});
-    }
-    res.render('cm.ejs', {
-      name: person.name,
-      cm: person.cm.toFixed(1)
-    });
-  })
-  .catch(err => {
-    console.error(err);
-  });
+  if(!name || name==""){
+      res.render('index.ejs', {});
+  } else {
+      Person.findOne({name}).exec()
+          .then(person => {
+              if(!person){
+                  person = new Person({
+                      name,
+                      cm: getRandomArbitrary(5, 20)
+                  });
+                  person.save()
+                      .then(()=>{;})
+                      .catch(err=>{console.error(err)});
+              }
+              res.render('cm.ejs', {
+                  name: person.name,
+                  cm: person.cm.toFixed(1)
+              });
+          })
+          .catch(err => {
+              console.error(err);
+          });
+  }
 });
 
 app.use(express.static('static'));
